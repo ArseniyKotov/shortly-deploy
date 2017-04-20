1ab54c82745780227518f3490c45d11c40c7f3b8
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-04-20 11:28:50 -0700

    took ip out

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var app = require('./server-config.js');
 
 var port = process.env.PORT || 4568;
 
-var ip = '162.243.44.239';
 
-
-app.listen(port, ip);
+app.listen(port);
 
 console.log('Server now listening on port ' + port);
 console.log('Server using ip ' + ip);
