20c2219158d5b9d42ebf764683c3ee0d9501a7d6
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-04-20 13:27:08 -0700

    Final commit. Fixed nav to link

--- app/models/user.js
+++ app/models/user.js
@@ -16,33 +16,29 @@ var UserSchema = new Schema({
   }
 });
 
-UserSchema.pre('save', function(next) {
-  var user = this;
-
-  // only hash the password if its new or updated
-  if (!user.isModified('password')) {
-    return next();
-  }
-    // hash the pw pleb
-  bcrypt.hash(user.password, null, null, function(err, hash) {
-    if (err) {
-      return next(err);
-    } else {
-      user.password = hash;
-      next();
-    }
-  });
-});
 
 UserSchema.methods.comparePassword = function(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
     if (err) {
-      return cb(err);
+      cb(err, null);
     } else {
       cb(null, isMatch);
     }
     
   });
 };
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+    // hash the pw pleb
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if (err) {
+      next(err);
+    } else {
+      user.password = hash;
+      next();
+    }
+  });
+});
 
 module.exports = mongoose.model('User', UserSchema);
