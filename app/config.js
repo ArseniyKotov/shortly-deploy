140a934576d145870416b9fe1d70d391671fa8c6
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-04-19 19:33:43 -0700

    added users and links mongoose refactor

--- app/config.js
+++ app/config.js
@@ -6,13 +6,12 @@ var dburl = 'mongodb://' + ip + ':' + port + '/shortly_deploy';
 
 mongoose.connect(dburl);
 
-
-function gracefulShutdown(msg, callback) {
+var gracefulShutdown = function(msg, callback) {
   mongoose.connection.close(function () {
     console.log('Mongoose disconnected through ' + msg);
     callback();
   });
-}
+};
 
 // For nodemon restarts
 process.once('SIGUSR2', function () {
@@ -35,46 +34,3 @@ process.on('SIGTERM', function () {
 
 var db = mongoose.connection;
 module.exports.db = db;
-
-
-
-// var path = require('path');
-// var knex = require('knex')({
-//   client: 'sqlite3',
-//   connection: {
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   },
-//   useNullAsDefault: true
-// });
-// var db = require('bookshelf')(knex);
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('baseUrl', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// module.exports = db;
--- app/models/link.js
+++ app/models/link.js
@@ -1,3 +1,19 @@
 var db = require('../config');
 var crypto = require('crypto');
 var mongoose = require('mongoose');
+
+LinkSchema = mongoose.Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: Number
+});
+
+LinkSchema.pre('save', function (next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
+module.exports = mongoose.model('Link', LinkSchema);
